f1c9930594823a887361fab366de693c18f339ef
fedacking <fedacking@gmail.com>
2025-04-06 18:08:40 -0300

    IT WORKS BABY

--- src/main.rs
+++ src/main.rs
@@ -7,105 +7,21 @@ use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::{self, BufRead};
 use std::path::Path;
+use delaunator::{Point, triangulate};
+extern crate geo;
+extern crate line_intersection;
 
-const INTERFACE_MULT: f32 = 10.0;
-const INTERFACE_OFFSET: f32 = 0.0;
 
-#[derive(Debug, Clone)]
-struct DelaunayTriangulation {
-    triangles: Vec<NavTriangle>,
-    edges: HashSet<Edge>,
-}
-
-impl DelaunayTriangulation {
-    fn new() -> Self {
-        Self {
-            triangles: Vec::new(),
-            edges: HashSet::new(),
-        }
-    }
-
-    fn triangulate(&mut self, points: &Vec<Vec2>) {
-        self.triangles.clear();
-        self.edges.clear();
-        let bounding_triangle = NavTriangle::from_coordinates([
-            Vec2 {
-                x: -10000.0,
-                y: 10000.0,
-            }, //TODO: make it so that it creates a triangle based on the points
-            Vec2 {
-                x: 10000.0,
-                y: 10000.0,
-            },
-            Vec2 {
-                x: 0.0,
-                y: -10000.0,
-            },
-        ]);
-
-        self.triangles.push(bounding_triangle);
-
-        for point in points {
-            let mut bad_triangles = Vec::new();
-
-            for triangle in &self.triangles {
-                if triangle.point_in_circumcircle(*point) {
-                    bad_triangles.push(*triangle);
-                }
-            }
-
-            let mut polygon = HashSet::new();
-            for triangle in &bad_triangles {
-                let edges = vec![
-                    Edge::new(triangle.coordinates[0], triangle.coordinates[1]),
-                    Edge::new(triangle.coordinates[1], triangle.coordinates[2]),
-                    Edge::new(triangle.coordinates[2], triangle.coordinates[0]),
-                ];
-
-                for edge in edges {
-                    if !polygon.insert(edge) {
-                        polygon.remove(&edge);
-                    }
-                }
-            }
-
-            self.triangles.retain(|t| !bad_triangles.contains(t));
-
-            for edge in polygon {
-                self.triangles
-                    .push(NavTriangle::from_coordinates([edge.0, edge.1, *point]));
-            }
-        }
-
-        self.triangles
-            .retain(|t| !t.triangle_share_point(&bounding_triangle));
-
-        for triangle in &self.triangles {
-            self.edges.insert(Edge::new(triangle.coordinates[0], triangle.coordinates[1]));
-            self.edges.insert(Edge::new(triangle.coordinates[1], triangle.coordinates[2]));
-            self.edges.insert(Edge::new(triangle.coordinates[2], triangle.coordinates[0]));
-        }
-    }
-
-    pub fn check_edge(&self, edge: &Edge) -> Vec<Vec2> {
-        let mut vec = Vec::new();
-        for other in &self.edges {
-            match edge.get_intersection_point(&other) {
-                Some(point) => { vec.push(point); },
-                None => {}
-            }
-        }
-        vec
-    }
-}
+const INTERFACE_MULT: f64 = 10.0;
+const INTERFACE_OFFSET: f64 = 0.0;
 
 #[derive(Debug, Clone)]
 struct Map {
     triangles: Vec<NavTriangle>,
-    non_passable_polygons: Vec<Vec<Vec2>>,
+    non_passable_polygons: Vec<Vec<Point>>,
 }
 
-fn check_triangle_in_polygons(t: &NavTriangle, polygons: &Vec<Vec<Vec2>>) -> bool {
+fn check_triangle_in_polygons(t: &NavTriangle, polygons: &Vec<Vec<Point>>) -> bool {
     for polygon in polygons {
         if polygon.contains(&t.coordinates[0])
             && polygon.contains(&t.coordinates[1])
@@ -117,68 +33,121 @@ fn check_triangle_in_polygons(t: &NavTriangle, polygons: &Vec<Vec<Vec2>>) -> boo
     false
 }
 
+fn indexes_to_triangles(points: &Vec<Point>, triangles: &Vec<usize>) -> Vec<NavTriangle> {
+    let mut index = 0;
+    let mut result = Vec::new();
+    while index + 2 < triangles.len() {
+        result.push(NavTriangle::from_coordinates([
+            points[triangles[index]].clone(),
+            points[triangles[index +1]].clone(),
+            points[triangles[index + 2]].clone(),
+        ]));
+        index += 3;
+    }
+    result
+}
+
+fn indexes_to_edges(points: &Vec<Point>, triangles: &Vec<usize>) -> HashSet<Edge> {
+    let mut index = 0;
+    let mut result = HashSet::new();
+    while index + 2 < triangles.len() {
+        result.insert(Edge::new(
+            points[triangles[index]].clone().into(),
+            points[triangles[index +1]].clone().into(),
+        ));
+        result.insert(Edge::new(
+            points[triangles[index]].clone().into(),
+            points[triangles[index +2]].clone().into(),
+        ));
+        result.insert(Edge::new(
+            points[triangles[index+1]].clone().into(),
+            points[triangles[index +2]].clone().into(),
+        ));
+        index += 3;
+    }
+    result
+}
+
 impl Map {
-    pub fn from_npp(mut non_passable_polygons: Vec<Vec<Vec2>>) -> Self {
-        let mut ref_polygons = &non_passable_polygons;
-        let mut combined_points: Vec<Vec2> = non_passable_polygons
+    pub fn from_npp(non_passable_polygons: &Vec<Vec<Point>>) -> Self {
+        let mut clone_polygons = non_passable_polygons.clone();
+        let mut combined_points: Vec<Point> = non_passable_polygons
             .clone()
             .into_iter()
             .flatten()
             .collect();
-        combined_points.push(Vec2 { x: 0.0, y: 60.0 });
-        combined_points.push(Vec2 { x: 1000.0, y: 60.0 });
-        combined_points.push(Vec2 { x: 0.0, y: 1000.0 });
-        combined_points.push(Vec2 {
+        combined_points.push(Point { x: 0.0, y: 60.0 });
+        combined_points.push(Point { x: 1000.0, y: 60.0 });
+        combined_points.push(Point { x: 0.0, y: 1000.0 });
+        combined_points.push(Point {
             x: 1000.0,
             y: 1000.0,
         });
-        let mut triangulation = DelaunayTriangulation::new();
-        triangulation.triangulate(&combined_points);
-        let mut triangles = triangulation.triangles.clone();
+        let mut triangulation = triangulate(&*combined_points);
+        let mut triangles = indexes_to_triangles(&combined_points, &triangulation.triangles);
+        let mut hash_edges: HashSet<Edge> = indexes_to_edges(&combined_points, &triangulation.triangles);
+        //println!("{:?}", triangles);
 
-        let mut new_non_passable_polygons = non_passable_polygons.clone();
+        let mut flag = true;
+        while flag {
+            flag = false;
 
-        let mut flag = 2;
-        while flag > 0{
-            flag -= 1;
-            for (polygon_index, polygon) in ref_polygons.clone().into_iter().enumerate() {
+            for (polygon_index, polygon) in clone_polygons.clone().into_iter().enumerate() {
                 for (i, point) in polygon.iter().enumerate() {
                     let other_index = (i + 1) % polygon.len();
-                    let edge = Edge::new(*point, polygon[other_index]);
-                    let extra_points = triangulation.check_edge(&edge);
+                    let edge = Edge::new(point.clone(), (&polygon[other_index]).clone());
+                    let extra_points = edge.get_intersection_points(&hash_edges);
                     for (offset, extra_point) in extra_points.iter().enumerate() {
-                        &new_non_passable_polygons[polygon_index].insert(other_index + offset, *extra_point);
-                        combined_points.push(*extra_point);
-                        print!("{extra_point:?}")
+                        clone_polygons[polygon_index].insert(other_index + offset, extra_point.clone());
+                        combined_points.push(extra_point.clone());
+                        flag = true;
                     }
                 }
             }
-            println!();
-            ref_polygons = &new_non_passable_polygons;
-            triangulation.triangulate(&combined_points);
+            let mut triangulation = triangulate(&*combined_points);
+            triangles = indexes_to_triangles(&combined_points, &triangulation.triangles);
+            hash_edges = indexes_to_edges(&combined_points, &triangulation.triangles);
         }
-        triangles = triangulation.triangles.clone();
+        // while flag > 0{
+        //     flag -= 1;
+        //     for (polygon_index, polygon) in ref_polygons.clone().into_iter().enumerate() {
+        //         for (i, point) in polygon.iter().enumerate() {
+        //             let other_index = (i + 1) % polygon.len();
+        //             let edge = Edge::new(*point, polygon[other_index]);
+        //             let extra_points = triangulation.check_edge(&edge);
+        //             for (offset, extra_point) in extra_points.iter().enumerate() {
+        //                 &new_non_passable_polygons[polygon_index].insert(other_index + offset, *extra_point);
+        //                 combined_points.push(*extra_point);
+        //                 print!("{extra_point:?}")
+        //             }
+        //         }
+        //     }
+        //     println!();
+        //     ref_polygons = &new_non_passable_polygons;
+        //     triangulation.triangulate(&combined_points);
+        // }
+        // triangles = triangulation.triangles.clone();
 
         //triangles.retain(|t| !check_triangle_in_polygons(t, &non_passable_polygons));
         Map {
             triangles,
-            non_passable_polygons,
+            non_passable_polygons: clone_polygons,
         }
     }
 }
 
 fn draw_nav_triangle(nav_triangle: &NavTriangle) {
     let v1 = Vec2 {
-        x: nav_triangle.coordinates[0][0],
-        y: nav_triangle.coordinates[0][1],
+        x: nav_triangle.coordinates[0].x as f32,
+        y: nav_triangle.coordinates[0].y as f32,
     };
     let v2 = Vec2 {
-        x: nav_triangle.coordinates[1][0],
-        y: nav_triangle.coordinates[1][1],
+        x: nav_triangle.coordinates[1].x as f32,
+        y: nav_triangle.coordinates[1].y as f32,
     };
     let v3 = Vec2 {
-        x: nav_triangle.coordinates[2][0],
-        y: nav_triangle.coordinates[2][1],
+        x: nav_triangle.coordinates[2].x as f32,
+        y: nav_triangle.coordinates[2].y as f32,
     };
     draw_line(v1.x, v1.y, v2.x, v2.y, 2.0, BLACK);
     draw_line(v1.x, v1.y, v3.x, v3.y, 2.0, BLACK);
@@ -186,7 +155,7 @@ fn draw_nav_triangle(nav_triangle: &NavTriangle) {
     draw_triangle(v1, v2, v3, GREEN);
 }
 
-fn read_file(filename: &str) -> io::Result<Vec<Vec<Vec2>>> {
+fn read_file(filename: &str) -> io::Result<Vec<Vec<Point>>> {
     let path = Path::new(filename);
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
@@ -195,16 +164,16 @@ fn read_file(filename: &str) -> io::Result<Vec<Vec<Vec2>>> {
     for line in reader.lines() {
         let line = line?; // Read the line
         let segments: Vec<&str> = line.split('-').collect();
-        let mut points: Vec<Vec2> = vec![];
+        let mut points: Vec<Point> = vec![];
 
         for segment in segments {
             let coordinates: Vec<&str> = segment.split(';').collect();
             if coordinates.len() == 2 {
-                let x: f32 =
+                let x: f64 =
                     coordinates[0].parse().unwrap_or(0.0) * INTERFACE_MULT + INTERFACE_OFFSET;
-                let y: f32 =
+                let y: f64 =
                     coordinates[1].parse().unwrap_or(0.0) * INTERFACE_MULT + INTERFACE_OFFSET;
-                points.push(Vec2 { x, y });
+                points.push(Point { x, y });
             }
         }
         vec.push(points);
@@ -214,8 +183,8 @@ fn read_file(filename: &str) -> io::Result<Vec<Vec<Vec2>>> {
 
 #[macroquad::main("Navmesh Visualizer")]
 async fn main() {
-    let mut vec = read_file("test_input/test2.fnav").unwrap();
-    let mut mesh = Map::from_npp(vec);
+    let vec = read_file("test_input/test2.fnav").unwrap();
+    let mut mesh = Map::from_npp(&vec);
     request_new_screen_size(1000.0, 1000.0); // horrible hardcoded stuff
     loop {
         clear_background(BLACK);
--- src/shape_math.rs
+++ src/shape_math.rs
@@ -1,11 +1,17 @@
+use std::collections::HashSet;
 use macroquad::math::Vec2;
 use std::hash::{Hash, Hasher};
+use delaunator::Point;
+use line_intersection::LineInterval;
+use geo::{Intersects, Line, LineIntersection};
+use geo::Coord;
+use geo::line_intersection::line_intersection;
 
-#[derive(Debug, Clone, Copy)]
-pub struct Edge(pub Vec2, pub Vec2);
+#[derive(Debug, Clone)]
+pub struct Edge(pub Point, pub Point);
 
 impl Edge {
-    pub fn new(v1: Vec2, v2: Vec2) -> Edge {
+    pub fn new(v1: Point, v2: Point) -> Edge {
         if v1.x < v2.x || (v1.x == v2.x && v1.y < v2.y) {
             return Edge(v1, v2);
         }
@@ -23,115 +29,80 @@ impl Eq for Edge {}
 
 impl Hash for Edge {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write_u32(self.0.x.to_bits());
-        state.write_u32(self.0.y.to_bits());
-        state.write_u32(self.1.x.to_bits());
-        state.write_u32(self.1.y.to_bits());
+        state.write_u64(self.0.x.to_bits());
+        state.write_u64(self.0.y.to_bits());
+        state.write_u64(self.1.x.to_bits());
+        state.write_u64(self.1.y.to_bits());
     }
 }
 
 impl Edge {
-    fn on_segment(&self, point: Vec2) -> bool {
-         (self.1.x > point.x) && (point.x > self.0.x) &&
-        ((self.1.y > point.y) && (point.y > self.0.y) ||
-         (self.1.y < point.y) && (point.y < self.0.y))
-    }
+    pub fn get_intersection_points(&self, others: &HashSet<Edge>) -> Vec<Point> {
+        let mut vec = vec![];
+        for other in others {
+            if other.0 == self.0 || other.1 == self.0 || other.0 == self.1 || other.1 == self.1 {
+                continue;
+            }
 
-    pub fn get_intersection_point(&self, other: &Edge) -> Option<Vec2> {
-        let a1 = self.1.y - self.0.y;
-        let b1 = self.0.x - self.1.x;
-        let c1 = a1 * self.1.x + b1 * self.1.y;
+            let segment = Line{
+                start:Coord{x: self.0.x, y: self.0.y },
+                end:Coord{x: self.1.x, y: self.1.y }
+            };
 
-        let a2 = other.1.y - other.0.y;
-        let b2 = other.0.x - other.1.x;
-        let c2 = a2 * other.0.x + b2 * other.0.y;
+            let other_segment = Line{
+                start:Coord{x: other.0.x, y: other.0.y },
+                end:Coord{x: other.1.x, y: other.1.y }
+            };
 
-        let determinant = a1 * b2 - a2 * b1;
+            match line_intersection(
+                segment,
+                other_segment,
+            ) {
+                Some(inter) => match inter {
+                    LineIntersection::SinglePoint{ intersection, is_proper} => {
+                        if is_proper {
+                            vec.push(Point{x: intersection.x, y: intersection.y});
+                        }
+                    },
+                    _ => {}
+                }
+                None => (),
+            }
 
-        if determinant.abs() < f32::EPSILON {
-            return None;
         }
-
-        let x = (b2 * c1 - b1 * c2) / determinant;
-        let y = (a1 * c2 - a2 * c1) / determinant;
-
-        let point = Vec2::new(x, y);
-        if self.on_segment(point) && other.on_segment(point) {
-            Some(point)
-        } else {
-            None
-        }
+        vec
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct NavTriangle {
-    pub coordinates: [Vec2; 3],
+    pub coordinates: [Point; 3],
 }
 
 impl NavTriangle {
-    pub fn from_coordinates(mut coordinates: [Vec2; 3]) -> NavTriangle {
+    pub fn from_coordinates(mut coordinates: [Point; 3]) -> NavTriangle {
         let det = coordinates[0].x * (coordinates[1].y - coordinates[2].y)
             + coordinates[1].x * (coordinates[2].y - coordinates[0].y)
             + coordinates[2].x * (coordinates[0].y - coordinates[1].y);
         if det > 0.0 {
-            (coordinates[1], coordinates[0]) = (coordinates[0], coordinates[1]);
+            NavTriangle { coordinates: [coordinates[1].clone(), coordinates[0].clone(), coordinates[2].clone()] }
+        } else {
+            NavTriangle { coordinates }
         }
-        NavTriangle { coordinates }
     }
 }
 
 impl PartialEq for NavTriangle {
     fn eq(&self, other: &Self) -> bool {
         let a = &self.coordinates;
-        let b = other.coordinates;
+        let b = &other.coordinates;
         (a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
             || (a[0] == b[1] && a[1] == b[2] && a[2] == b[0])
             || (a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
     }
 }
 
-impl NavTriangle {
-    pub fn point_in_triangle(&self, point: Vec2) -> bool {
-        let v0 = self.coordinates[1] - self.coordinates[0];
-        let v1 = self.coordinates[2] - self.coordinates[0];
-        let v2 = point - self.coordinates[0];
-
-        let d00 = v0.dot(v0);
-        let d01 = v0.dot(v1);
-        let d11 = v1.dot(v1);
-        let d20 = v2.dot(v0);
-        let d21 = v2.dot(v1);
-
-        let denom = d00 * d11 - d01 * d01;
-        if denom == 0.0 {
-            return false; // Degenerate triangle
-        }
-
-        let v = (d11 * d20 - d01 * d21) / denom;
-        let w = (d00 * d21 - d01 * d20) / denom;
-        let u = 1.0 - v - w;
-
-        u >= 0.0 && v >= 0.0 && w >= 0.0
-    }
-
-    pub fn point_in_circumcircle(&self, point: Vec2) -> bool {
-        let [a, b, c] = self.coordinates;
-
-        let ax = a.x - point.x;
-        let ay = a.y - point.y;
-        let bx = b.x - point.x;
-        let by = b.y - point.y;
-        let cx = c.x - point.x;
-        let cy = c.y - point.y;
-
-        let det = (ax * ax + ay * ay) * (bx * cy - by * cx)
-            - (bx * bx + by * by) * (ax * cy - ay * cx)
-            + (cx * cx + cy * cy) * (ax * by - ay * bx);
-
-        det < 0.0
-    }
-
+ impl NavTriangle {
     pub fn triangle_share_point(&self, other: &NavTriangle) -> bool {
         self.coordinates[0] == other.coordinates[0]
             || self.coordinates[0] == other.coordinates[1]
