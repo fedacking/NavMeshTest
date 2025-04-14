c68e58504ba3bd2a5bb1ea19d071ca57bb942a73
fedacking <fedacking@gmail.com>
2025-04-14 01:00:34 -0300

    path drawing

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ use std::io;
 use std::io::BufRead;
 use std::path::Path;
 use macroquad::prelude::*;
-use geo::{Coord, LineString, Point, Triangle};
+use geo::{coord, Coord, LineString, Point, Triangle};
+use crate::nav_mesh::NavMesh;
 use crate::triangulator::from_npp;
 
-fn draw_nav_triangle(triangle: &Triangle) {
+fn draw_nav_triangle(triangle: &Triangle, color: Color) {
     let [l1, l2, l3] = triangle.to_array();
     let v1 = Vec2 { x: l1.x as f32, y: l1.y as f32 };
     let v2 = Vec2 { x: l2.x as f32, y: l2.y as f32 };
@@ -18,8 +19,13 @@ fn draw_nav_triangle(triangle: &Triangle) {
     draw_line(v1.x, v1.y, v2.x, v2.y, 2.0, BLACK);
     draw_line(v1.x, v1.y, v3.x, v3.y, 2.0, BLACK);
     draw_line(v3.x, v3.y, v2.x, v2.y, 2.0, BLACK);
-    draw_triangle(v1, v2, v3, GREEN);
+    draw_triangle(v1, v2, v3, color);
+}
 
+fn draw_path(triangle: &Vec<Triangle>, path: &Vec<usize>) {
+    for point in path {
+        draw_nav_triangle(&triangle[*point], BLUE);
+    }
 }
 
 fn read_file(filename: &str) -> io::Result<(Vec<Point>, Vec<LineString>)> {
@@ -67,13 +73,15 @@ async fn main() {
     let file_data = read_file("test_input/test2.fnav").unwrap();
     let mut points = file_data.0;
     let mut constraints = file_data.1;
-    let triangles = from_npp(&mut points, &mut constraints);
+    let navmesh = NavMesh::new(&mut points, &mut constraints);
+    let path = navmesh.path(&coord! {x: 1.0, y: 1.0}, &coord! {x: 999.0, y: 999.0}).unwrap();
     request_new_screen_size(1000.0, 1000.0); // horrible hardcoded stuff
     loop {
         clear_background(BLACK);
-        for triangle in &triangles {
-            draw_nav_triangle(triangle);
+        for triangle in &navmesh.triangles {
+            draw_nav_triangle(triangle, GREEN);
         }
+        draw_path(&navmesh.triangles, &path.0);
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, WHITE);
         next_frame().await
     }
--- src/nav_mesh.rs
+++ src/nav_mesh.rs
@@ -1,11 +1,85 @@
-use geo::{Point, Polygon, Triangle};
+use std::ops::{Add, Div};
+use geo::{Contains, Coord, EuclideanDistance, LineString, Point, Triangle};
+use pathfinding::prelude::astar;
+use crate::triangulator::from_npp;
+
+pub fn check_edge_in_triangle(o1: Coord, o2: Coord, triangle: &Triangle) -> bool {
+    (o1 == triangle.0 || o1 == triangle.1 || o1 == triangle.2) &&
+    (o2 == triangle.0 || o2 == triangle.1 || o2 == triangle.2)
+}
+
+fn make_graph(triangles: &Vec<Triangle>) -> Vec<Vec<(usize, u64)>> {
+    let mut vec = Vec::new();
+
+    for triangle in triangles {
+        let mut inner_vec: Vec<(usize, u64)> = Vec::new();
+        let [v1, v2, v3] = [triangle.0, triangle.1, triangle.2];
+        let center = v1.add(v2).add(v3).div(3.0);
+        for (i, other_triangle) in triangles.iter().enumerate() {
+            if other_triangle == triangle { continue }
+            if check_edge_in_triangle(v1, v2, other_triangle) ||
+                check_edge_in_triangle(v2, v3, other_triangle) ||
+                check_edge_in_triangle(v3, v1, other_triangle) {
+                let [o1, o2, o3] = [other_triangle.0, other_triangle.1, other_triangle.2];
+                let o_center = o1.add(o2).add(o3).div(3.0);
+
+                let distance = center.euclidean_distance(&o_center);
+
+                inner_vec.push((i, distance as u64));
+            }
+        }
+        vec.push(inner_vec);
+    };
+    vec
+}
 
 pub struct NavMesh {
     pub triangles: Vec<Triangle>,
+    pub graph: Vec<Vec<(usize, u64)>>,
 }
 
-pub fn vec(points: &Vec<Point>, polygons: &Vec<Polygon>) -> NavMesh {
-    NavMesh {
-        triangles: vec![]
+impl NavMesh {
+    pub fn new(mut points: &mut Vec<geo::Point>, mut constraints: &mut Vec<LineString>) -> NavMesh {
+        let triangles = from_npp(points, constraints);
+        let graph = make_graph(&triangles);
+
+        NavMesh {
+            triangles,
+            graph,
+        }
+    }
+
+    fn find_triangle(&self, coord: &Coord) -> Option<usize> {
+        for (i, triangle) in self.triangles.iter().enumerate() {
+            if triangle.contains(coord) {
+                return Some(i);
+            }
+        }
+        None
+    }
+
+    fn find_successors(&self, index: &usize) -> Vec<(usize, u64)> {
+        self.graph[*index].clone()
+    }
+
+    fn heuristic(&self, index_1: &usize, index_2: &usize) -> u64 {
+        let triangle_1 = &self.triangles[*index_1];
+        let triangle_2 = &self.triangles[*index_2];
+
+        let [v1, v2, v3] = [triangle_1.0, triangle_1.1, triangle_1.2];
+        let center = v1.add(v2).add(v3).div(3.0);
+        let [o1, o2, o3] = [triangle_2.0, triangle_2.1, triangle_2.2];
+        let o_center = v1.add(v2).add(v3).div(3.0);
+        center.euclidean_distance(&o_center) as u64
+    }
+
+    pub fn path(&self, coord1: &Coord, coord2: &Coord) -> Option<(Vec<usize>, u64)> {
+        let index_1 = self.find_triangle(coord1).unwrap();
+        let index_2 = self.find_triangle(coord2).unwrap();
+
+        let path = astar(&index_1, |i| { self.find_successors(i) },
+                         |i| { self.heuristic(i, &index_2) },
+                         | succ | { index_2 == *succ });
+        path
     }
 }
